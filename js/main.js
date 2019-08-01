81e07c290c0f76b0d503a1d88e6a37ae14df0c05
Eric Chou <ericchou@qnap.com>
2019-08-01 17:12:28 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 function initPhotoDesc(){
+	$(".post-body img").eq(0).after(
+	'<!-- GGMant_側邊欄廣告 -->\
+	<div style="width:660px;">\
+	<ins class="adsbygoogle"\
+	     style="display:inline-block;width:300px;height:250px;margin-right:30px"\
+	     data-ad-client="ca-pub-8384162002444497"\
+	     data-ad-slot="1767154162"></ins>\
+	<ins class="adsbygoogle"\
+	     style="display:inline-block;width:300px;height:250px;margin-left:30px"\
+	     data-ad-client="ca-pub-8384162002444497"\
+	     data-ad-slot="1767154162"></ins>\
+	</div>');(adsbygoogle = window.adsbygoogle || []).push({}); (adsbygoogle = window.adsbygoogle || []).push({}); 
+
 	var index=0;
 	$(".post-body img").each(function(){
 		var width=$(this).attr('title');
@@ -22,15 +35,13 @@ function initPhotoDesc(){
 				'<ins class="adsbygoogle" data-ad-client="ca-pub-8384162002444497" data-ad-format="auto" data-ad-slot="1452756561" style="display: block;"></ins>'+
 				'</div>'+
 				'<!---- GGM 內文廣告:end ---->';
-
+				
 		}
 
 		$(this)[0].outerHTML=html+addTitle+addAD;
-		
+		if(addAD)(adsbygoogle = window.adsbygoogle || []).push({}); 
 	});
-
-	//init All Ads.
-	(adsbygoogle = window.adsbygoogle || []).push({}); 
+	
 }
 
 
