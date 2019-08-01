522f4d6561a587d64fc306c424720bcc5d6b3ab2
Eric Chou <ericchou@qnap.com>
2019-08-01 12:32:34 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 function initPhotoDesc(){
 	$(".post-body img").each(function(){
+		var width=$(this).attr('title');
+		if(width){
+			$(this).width(width)
+		}
 		var title=$(this).attr('alt');
 		var html=$(this)[0].outerHTML;
-		$(this)[0].outerHTML=html+'<p class="photo-desc">“ '+title+' ”</p>';
+		if(title&&title.trim()){
+			$(this)[0].outerHTML=html+'<p class="photo-desc">“ '+title+' ”</p>';
+		}
+		
 	});
 }
